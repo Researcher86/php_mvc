8f65c4b43c0c4bbb1a58e0eefb3c2ba5d3a6f906
Tanat <researcher86@mail.ru>
2016-04-04 15:51:21 +0600

    Рефакторинг

--- Core/Exceptions/AppException.php
+++ Core/Exceptions/AppException.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Comp
- * Date: 04.04.2016
- * Time: 15:36
- */
 
 namespace Core\Exceptions;
 
 
 class AppException extends \Exception
 {
-
+    // NOP
 }
