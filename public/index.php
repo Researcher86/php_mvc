9f6cf1bc5c8d15bd167f8ad94ecc9208559b3da9
Tanat <researcher86@mail.ru>
2016-04-04 15:49:22 +0600

    Выводим сообщение исключительной ситуации

--- public/index.php
+++ public/index.php
@@ -14,15 +14,19 @@ try {
     (new \Core\Router($_SERVER['REQUEST_URI']))->run();
 } catch (\Core\Exceptions\E404Exception $e) {
     //TODO: Logging error page not found 404
+    echo $e->getMessage();
     throw $e;
 } catch (\Core\Exceptions\ModelException $e) {
     //TODO: Logging error business logic 500
+    echo $e->getMessage();
     throw $e;
 } catch (\Core\Exceptions\DbException $e) {
     //TODO: Logging error db 500
+    echo $e->getMessage();
     throw $e;
 } catch (\Exception $e) {
     //TODO: Logging... 500
+    echo $e->getMessage();
     throw $e;
 }
 
