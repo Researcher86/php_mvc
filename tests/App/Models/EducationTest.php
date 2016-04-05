c99dea73e99873ba80854c9a200a3820b35a73a5
Tanat <researcher86@mail.ru>
2016-04-05 09:47:56 +0600

    Рефакторинг

--- tests/App/Models/EducationTest.php
+++ tests/App/Models/EducationTest.php
@@ -2,14 +2,6 @@
 
 namespace App\Models;
 
-use Core\Config;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Tanat
- * Date: 04.04.2016
- * Time: 12:44
- */
 class EducationTest extends \PHPUnit_Framework_TestCase
 {
 
