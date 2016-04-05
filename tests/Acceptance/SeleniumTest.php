c521ca1332e6fac0733dfd871c8b5271d41b6e68
Tanat <researcher86@mail.ru>
2016-04-05 09:48:48 +0600

    + приемочное тестирование

--- tests/Acceptance/SeleniumTest.php
+++ tests/Acceptance/SeleniumTest.php
@@ -1,13 +1,5 @@
 <?php
 
-
-
-/**
- * Created by IntelliJ IDEA.
- * User: Tanat
- * Date: 04.04.2016
- * Time: 13:41
- */
 class SeleniumTest extends \PHPUnit_Extensions_Selenium2TestCase
 {
     public function setUp()
