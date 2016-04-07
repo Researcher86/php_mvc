602390af5e4d0b63fda111879780d5902fb37700
Tanat <researcher86@mail.ru>
2016-04-07 11:56:29 +0600

    + тестирование класса User

--- test/App/Models/UserTest.php
+++ test/App/Models/UserTest.php
@@ -2,11 +2,47 @@
 
 namespace App\Models;
 
-class UserTest extends \PHPUnit_Framework_TestCase
+class UserTest extends BaseTest
 {
-
     public function testGetAll()
     {
-        User::getAll();
+        $users = User::getAll();
+        $this->assertEquals(2, count($users));
     }
+
+    public function testCreate()
+    {
+        $user = new User();
+        $user->firstName = 'Танат';
+        $user->lastName = 'Альпенов';
+        $user->patronymic = 'Маратович';
+        $user->yearOfBirth = '1986-07-22';
+        $user->sex = 1;
+        $user->education_id = 2;
+        $user->email = 'researcher2286@gmail.com';
+        $user->password = '123456';
+
+        $user->save();
+
+        $storeUser = User::getById($user->id);
+        $this->assertEquals($user->email, $storeUser->email);
+    }
+
+
+    public function testDelete()
+    {
+        $storeUser = User::getById(1);
+
+        $storeUser->delete();
+
+        $storeUser = User::getById(1);
+        $this->assertNull($storeUser);
+    }
+
+    public function testGetByEmail()
+    {
+        $user = User::getByEmail('researcher86@mail.ru');
+        $this->assertEquals(1, count($user));
+    }
+
 }
