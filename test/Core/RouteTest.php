f8085117069513f9d5459e5903c35023bdb02b8a
Tanat <researcher86@mail.ru>
2016-04-07 11:51:56 +0600

    Блокирует вывод рендеринг страницы

--- test/Core/RouteTest.php
+++ test/Core/RouteTest.php
@@ -9,12 +9,16 @@ class RouteTest extends \PHPUnit_Framework_TestCase
     {
         $_SERVER['REQUEST_URI'] = '/index';
         $route = new Route($_SERVER['REQUEST_URI']);
+        ob_start();
         $route->run();
+        ob_clean();
     }
 
     public function testSubDirControllers()
     {
         $route = new Route('/admin/index');
+        ob_start();
         $route->run();
+        ob_clean();
     }
 }
